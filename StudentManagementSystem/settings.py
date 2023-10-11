14acd98c9118f43e7c8e25d1a4c0c0c10408f379
Abdul Moiz Sarwar <abdulmoizsarwar@gmail.com>
2023-10-12 02:21:02 +0500

    change in spells

--- StudentManagementSystem/settings.py
+++ StudentManagementSystem/settings.py
@@ -113,4 +113,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 import os
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
