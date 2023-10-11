54b290270e5eff4331c2fea6211477f865ff969c
Abdul Moiz Sarwar <abdulmoizsarwar@gmail.com>
2023-10-12 01:50:38 +0500

    new vercel files with static files update

--- StudentManagementSystem/urls.py
+++ StudentManagementSystem/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from . import settings
 
 urlpatterns = [
     path('', admin.site.urls),
 ]
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
